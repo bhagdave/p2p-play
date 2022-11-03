6fcffcdf7bab3f6d47a39d0e261820b9c30988f8
Dave Gill <david.g.h.gill@gmail.com>
2022-11-03 21:47:23 +0000

    Building transport for the P2P

---
+++ notes.md
@@ -0,0 +1,1 @@
+This seems to be a way of doing the transport in the create_transport function. https://github.com/pyrsia/pyrsia/blob/main/src/network/p2p.rs
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use libp2p::floodsub::Topic;
 use libp2p::identity;
 use libp2p::PeerId;
+use libp2p::mplex;
+use libp2p::core::upgrade;
+use libp2p::tcp;
+use libp2p::dns;
+use libp2p::Transport;
+use libp2p::tcp::GenTcpConfig;
+use libp2p::noise::{Keypair, NoiseConfig, X25519Spec};
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 use log::{info};
@@ -49,9 +56,23 @@ enum EventType {
 
 #[tokio::main]
 async fn main(){
-    pretty_env_logger::init();
+    pretty_env_logger::init(); // Start up the logging - Only works whwn RUST_LOG=trace is in the environment
 
     info!("Peer id: {}", PEER_ID.clone());
+    // CReate an async channel for comms
     let (response_sender, response_rcv) : (tokio::sync::mpsc::UnboundedSender<ListResult>, tokio::sync::mpsc::UnboundedReceiver<ListResult>)  = mpsc::unbounded_channel();
+    // setup keys for securing traffic
+    let auth_keys = Keypair::<X25519Spec>::new()
+        .into_authentic(&KEYS) // Sign with our keys from earlier
+        .expect("can create auth keys");
+    
+    // Lets create the transport layer
+    let tcp = GenTcpConfig::new().nodelay(true);
+    let transport = tcp::TokioTcpTransport::new(tcp);
+    let dns = dns::TokioDnsConfig::system(transport)?;
+    dns.upgrade(upgrade::Version::V1)
+        .authenticate(NoiseConfig::xx(auth_keys).into_authenticated())
+        .multiplex(mplex::MplexConfig::new())
+        .boxed();
 }
 
