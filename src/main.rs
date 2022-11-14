2e94c8e6aa865207b9c56d140c7e297d78b9e63f
Dave Gill <david.g.h.gill@gmail.com>
2022-11-14 19:50:31 +0000

    Got two working

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,35 @@
-use libp2p::floodsub::Topic;
-use libp2p::identity;
-use libp2p::PeerId;
-use libp2p::mplex;
-use libp2p::core::upgrade;
-use libp2p::tcp;
-use libp2p::dns;
-use libp2p::Transport;
-use libp2p::tcp::GenTcpConfig;
-use libp2p::noise::{Keypair, NoiseConfig, X25519Spec};
+use libp2p::{
+    core::upgrade,
+    floodsub::{Floodsub, FloodsubEvent, Topic},
+    futures::StreamExt,
+    identity,
+    mdns::{Mdns, MdnsEvent},
+    mplex,
+    noise::{Keypair, NoiseConfig, X25519Spec},
+    swarm::{NetworkBehaviourEventProcess, Swarm, SwarmBuilder},
+    tcp::TokioTcpConfig,
+    NetworkBehaviour, PeerId, Transport,
+};
+use log::{error, info};
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
-use log::{info};
-use tokio::sync::mpsc;
-use libp2p::mdns::TokioMdns;
-use libp2p::floodsub::Floodsub;
+use std::collections::HashSet;
+use tokio::{fs, io::AsyncBufReadExt, sync::mpsc};
 
-const STORAGE_FILE_PATH: &str = "./data.json";
+const STORAGE_FILE_PATH: &str = "./stories.json";
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
-
-static KEYS: Lazy<identity::Keypair> = Lazy::new(|| identity::Keypair::generate_ed25519()); // Generate a key pair using once_cell:Lazy
-static PEER_ID: Lazy<PeerId> = Lazy::new(|| PeerId::from(KEYS.public())); // Genereate a peer id from the public key
-static TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("stories")); // generate a topic to listen to on the network
-
 type Stories = Vec<Story>;
+
+static KEYS: Lazy<identity::Keypair> = Lazy::new(|| identity::Keypair::generate_ed25519());
+static PEER_ID: Lazy<PeerId> = Lazy::new(|| PeerId::from(KEYS.public()));
+static TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("stories"));
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Story {
     id: usize,
-    title: String,
+    name: String,
+    header: String,
     body: String,
     public: bool,
 }
@@ -45,45 +46,290 @@ struct ListRequest {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct ListResult {
+struct ListResponse {
     mode: ListMode,
     data: Stories,
     receiver: String,
 }
 
 enum EventType {
-    Response(ListResult),
+    Response(ListResponse),
     Input(String),
 }
 
-#[tokio::main]
-async fn main(){
-    pretty_env_logger::init(); // Start up the logging - Only works whwn RUST_LOG=trace is in the environment
+#[derive(NetworkBehaviour)]
+struct StoryBehaviour {
+    floodsub: Floodsub,
+    mdns: Mdns,
+    #[behaviour(ignore)]
+    response_sender: mpsc::UnboundedSender<ListResponse>,
+}
 
-    info!("Peer id: {}", PEER_ID.clone());
-    // CReate an async channel for comms
-    let (response_sender, response_rcv) : (tokio::sync::mpsc::UnboundedSender<ListResult>, tokio::sync::mpsc::UnboundedReceiver<ListResult>)  = mpsc::unbounded_channel();
-    // setup keys for securing traffic
+impl NetworkBehaviourEventProcess<FloodsubEvent> for StoryBehaviour {
+    fn inject_event(&mut self, event: FloodsubEvent) {
+        match event {
+            FloodsubEvent::Message(msg) => {
+                if let Ok(resp) = serde_json::from_slice::<ListResponse>(&msg.data) {
+                    if resp.receiver == PEER_ID.to_string() {
+                        info!("Response from {}:", msg.source);
+                        resp.data.iter().for_each(|r| info!("{:?}", r));
+                    }
+                } else if let Ok(req) = serde_json::from_slice::<ListRequest>(&msg.data) {
+                    match req.mode {
+                        ListMode::ALL => {
+                            info!("Received ALL req: {:?} from {:?}", req, msg.source);
+                            respond_with_public_stories(
+                                self.response_sender.clone(),
+                                msg.source.to_string(),
+                            );
+                        }
+                        ListMode::One(ref peer_id) => {
+                            if peer_id == &PEER_ID.to_string() {
+                                info!("Received req: {:?} from {:?}", req, msg.source);
+                                respond_with_public_stories(
+                                    self.response_sender.clone(),
+                                    msg.source.to_string(),
+                                );
+                            }
+                        }
+                    }
+                }
+            }
+            _ => (),
+        }
+    }
+}
+impl NetworkBehaviourEventProcess<MdnsEvent> for StoryBehaviour {
+    fn inject_event(&mut self, event: MdnsEvent) {
+        match event {
+            MdnsEvent::Discovered(discovered_list) => {
+                for (peer, _addr) in discovered_list {
+                    self.floodsub.add_node_to_partial_view(peer);
+                }
+            }
+            MdnsEvent::Expired(expired_list) => {
+                for (peer, _addr) in expired_list {
+                    if !self.mdns.has_node(&peer) {
+                        self.floodsub.remove_node_from_partial_view(&peer);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+fn respond_with_public_stories(sender: mpsc::UnboundedSender<ListResponse>, receiver: String) {
+    tokio::spawn(async move {
+        match read_local_stories().await {
+            Ok(stories) => {
+                let resp = ListResponse {
+                    mode: ListMode::ALL,
+                    receiver,
+                    data: stories.into_iter().filter(|r| r.public).collect(),
+                };
+                if let Err(e) = sender.send(resp) {
+                    error!("error sending response via channel, {}", e);
+                }
+            }
+            Err(e) => error!("error fetching local stories to answer ALL request, {}", e),
+        }
+    });
+}
+
+async fn create_new_story(name: &str, header: &str, body: &str) -> Result<()> {
+    let mut local_stories = read_local_stories().await?;
+    let new_id = match local_stories.iter().max_by_key(|r| r.id) {
+        Some(v) => v.id + 1,
+        None => 0,
+    };
+    local_stories.push(Story {
+        id: new_id,
+        name: name.to_owned(),
+        header: header.to_owned(),
+        body: body.to_owned(),
+        public: false,
+    });
+    write_local_stories(&local_stories).await?;
+
+    info!("Created story:");
+    info!("Name: {}", name);
+    info!("Header: {}", header);
+    info!("Body:: {}", body);
+
+    Ok(())
+}
+
+async fn publish_story(id: usize) -> Result<()> {
+    let mut local_stories = read_local_stories().await?;
+    local_stories
+        .iter_mut()
+        .filter(|r| r.id == id)
+        .for_each(|r| r.public = true);
+    write_local_stories(&local_stories).await?;
+    Ok(())
+}
+
+async fn read_local_stories() -> Result<Stories> {
+    let content = fs::read(STORAGE_FILE_PATH).await?;
+    let result = serde_json::from_slice(&content)?;
+    Ok(result)
+}
+
+async fn write_local_stories(stories: &Stories) -> Result<()> {
+    let json = serde_json::to_string(&stories)?;
+    fs::write(STORAGE_FILE_PATH, &json).await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    info!("Peer Id: {}", PEER_ID.clone());
+    let (response_sender, mut response_rcv) = mpsc::unbounded_channel();
+
     let auth_keys = Keypair::<X25519Spec>::new()
-        .into_authentic(&KEYS) // Sign with our keys from earlier
+        .into_authentic(&KEYS)
         .expect("can create auth keys");
-    
-    // Lets create the transport layer
-    let tcp = GenTcpConfig::new().nodelay(true); // The TCP Config
-    let transport = tcp::TokioTcpTransport::new(tcp); // The transport layer
-    let dns = dns::TokioDnsConfig::system(transport)?; // DNS
-    // Upgrade the connection to use noise for secure comms
-    // using NoiceConfig::xx because it will work with other libp2p apps
-    // Use multiplex to have multiple connections
-    dns.upgrade(upgrade::Version::V1)
-        .authenticate(NoiseConfig::xx(auth_keys).into_authenticated()) 
+
+    let transp = TokioTcpConfig::new()
+        .upgrade(upgrade::Version::V1)
+        .authenticate(NoiseConfig::xx(auth_keys).into_authenticated()) // XX Handshake pattern, IX exists as well and IK - only XX currently provides interop with other libp2p impls
         .multiplex(mplex::MplexConfig::new())
         .boxed();
 
-    let mut behaviour = RecipeBehaviour {
+    let mut behaviour = StoryBehaviour {
         floodsub: Floodsub::new(PEER_ID.clone()),
-        mdns: TokioMdns::new().expect("can create mdns"),
+        mdns: Mdns::new(Default::default())
+            .await
+            .expect("can create mdns"),
         response_sender,
+    };
+
+    behaviour.floodsub.subscribe(TOPIC.clone());
+
+    let mut swarm = SwarmBuilder::new(transp, behaviour, PEER_ID.clone())
+        .executor(Box::new(|fut| {
+            tokio::spawn(fut);
+        }))
+        .build();
+
+    let mut stdin = tokio::io::BufReader::new(tokio::io::stdin()).lines();
+
+    Swarm::listen_on(
+        &mut swarm,
+        "/ip4/0.0.0.0/tcp/0"
+            .parse()
+            .expect("can get a local socket"),
+    )
+    .expect("swarm can be started");
+
+    loop {
+        let evt = {
+            tokio::select! {
+                line = stdin.next_line() => Some(EventType::Input(line.expect("can get line").expect("can read line from stdin"))),
+                response = response_rcv.recv() => Some(EventType::Response(response.expect("response exists"))),
+                event = swarm.select_next_some() => {
+                    info!("Unhandled Swarm Event: {:?}", event);
+                    None
+                },
+            }
+        };
+
+        if let Some(event) = evt {
+            match event {
+                EventType::Response(resp) => {
+                    let json = serde_json::to_string(&resp).expect("can jsonify response");
+                    swarm
+                        .behaviour_mut()
+                        .floodsub
+                        .publish(TOPIC.clone(), json.as_bytes());
+                }
+                EventType::Input(line) => match line.as_str() {
+                    "ls p" => handle_list_peers(&mut swarm).await,
+                    cmd if cmd.starts_with("ls s") => handle_list_stories(cmd, &mut swarm).await,
+                    cmd if cmd.starts_with("create s") => handle_create_stories(cmd).await,
+                    cmd if cmd.starts_with("publish s") => handle_publish_story(cmd).await,
+                    _ => error!("unknown command"),
+                },
+            }
+        }
+    }
+}
+
+async fn handle_list_peers(swarm: &mut Swarm<StoryBehaviour>) {
+    info!("Discovered Peers:");
+    let nodes = swarm.behaviour().mdns.discovered_nodes();
+    let mut unique_peers = HashSet::new();
+    for peer in nodes {
+        unique_peers.insert(peer);
+    }
+    unique_peers.iter().for_each(|p| info!("{}", p));
+}
+
+async fn handle_list_stories(cmd: &str, swarm: &mut Swarm<StoryBehaviour>) {
+    let rest = cmd.strip_prefix("ls s ");
+    match rest {
+        Some("all") => {
+            let req = ListRequest {
+                mode: ListMode::ALL,
+            };
+            let json = serde_json::to_string(&req).expect("can jsonify request");
+            swarm
+                .behaviour_mut()
+                .floodsub
+                .publish(TOPIC.clone(), json.as_bytes());
+        }
+        Some(story_peer_id) => {
+            let req = ListRequest {
+                mode: ListMode::One(story_peer_id.to_owned()),
+            };
+            let json = serde_json::to_string(&req).expect("can jsonify request");
+            swarm
+                .behaviour_mut()
+                .floodsub
+                .publish(TOPIC.clone(), json.as_bytes());
+        }
+        None => {
+            match read_local_stories().await {
+                Ok(v) => {
+                    info!("Local stories ({})", v.len());
+                    v.iter().for_each(|r| info!("{:?}", r));
+                }
+                Err(e) => error!("error fetching local stories: {}", e),
+            };
+        }
     };
 }
 
+async fn handle_create_stories(cmd: &str) {
+    if let Some(rest) = cmd.strip_prefix("create s") {
+        let elements: Vec<&str> = rest.split("|").collect();
+        if elements.len() < 3 {
+            info!("too few arguments - Format: name|header|body");
+        } else {
+            let name = elements.get(0).expect("name is there");
+            let header = elements.get(1).expect("header is there");
+            let body = elements.get(2).expect("body is there");
+            if let Err(e) = create_new_story(name, header, body).await {
+                error!("error creating story: {}", e);
+            };
+        }
+    }
+}
+
+async fn handle_publish_story(cmd: &str) {
+    if let Some(rest) = cmd.strip_prefix("publish s") {
+        match rest.trim().parse::<usize>() {
+            Ok(id) => {
+                if let Err(e) = publish_story(id).await {
+                    info!("error publishing story with id {}, {}", id, e)
+                } else {
+                    info!("Published story with id: {}", id);
+                }
+            }
+            Err(e) => error!("invalid id: {}, {}", rest.trim(), e),
+        };
+    }
+}
