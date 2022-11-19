53b7f9452066a1a0f71ca53e329837c6d17e0c6f
Dave Gill <dave.gill@servicecertainty.co.uk>
2022-11-19 16:20:07 +0000

    First batch opf changes

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use libp2p::{
     mdns::{Mdns, MdnsEvent},
     mplex,
     noise::{Keypair, NoiseConfig, X25519Spec},
-    swarm::{NetworkBehaviourEventProcess, Swarm, SwarmBuilder},
+    swarm::{ Swarm, SwarmBuilder},
     tcp::TokioTcpTransport,
     NetworkBehaviour, PeerId, Transport,
 };
@@ -14,6 +14,7 @@ use log::{error, info};
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
+use libp2p::swarm::SwarmEvent;
 use libp2p::tcp::GenTcpConfig;
 use tokio::{fs, io::AsyncBufReadExt, sync::mpsc};
 
@@ -56,69 +57,74 @@ struct ListResponse {
 enum EventType {
     Response(ListResponse),
     Input(String),
+    FloodsubEvent(FloodsubEvent),
+    MdnsEvent(MdnsEvent)
 }
 
 #[derive(NetworkBehaviour)]
-#[behaviour(event_process = true)]
+#[behaviour(out_event = "StoryBehaviourEvent")]
 struct StoryBehaviour {
     floodsub: Floodsub,
     mdns: Mdns,
     #[behaviour(ignore)]
     response_sender: mpsc::UnboundedSender<ListResponse>,
 }
+#[derive(Debug)]
+enum StoryBehaviourEvent {
+    Floodsub(FloodsubEvent),
+    Mdns(MdnsEvent),
+}
 
-impl NetworkBehaviourEventProcess<FloodsubEvent> for StoryBehaviour {
-    fn inject_event(&mut self, event: FloodsubEvent) {
-        match event {
-            FloodsubEvent::Message(msg) => {
-                if let Ok(resp) = serde_json::from_slice::<ListResponse>(&msg.data) {
-                    if resp.receiver == PEER_ID.to_string() {
-                        info!("Response from {}:", msg.source);
-                        resp.data.iter().for_each(|r| info!("{:?}", r));
-                    }
-                } else if let Ok(req) = serde_json::from_slice::<ListRequest>(&msg.data) {
-                    match req.mode {
-                        ListMode::ALL => {
-                            info!("Received ALL req: {:?} from {:?}", req, msg.source);
-                            respond_with_public_stories(
-                                self.response_sender.clone(),
-                                msg.source.to_string(),
-                            );
-                        }
-                        ListMode::One(ref peer_id) => {
-                            if peer_id == &PEER_ID.to_string() {
-                                info!("Received req: {:?} from {:?}", req, msg.source);
-                                respond_with_public_stories(
-                                    self.response_sender.clone(),
-                                    msg.source.to_string(),
-                                );
-                            }
-                        }
-                    }
-                }
-            }
-            _ => (),
-        }
+impl From<FloodsubEvent> for StoryBehaviourEvent {
+    fn from(event: FloodsubEvent) -> Self {
+        StoryBehaviourEvent::Floodsub(event)
     }
 }
-impl NetworkBehaviourEventProcess<MdnsEvent> for StoryBehaviour {
-    fn inject_event(&mut self, event: MdnsEvent) {
-        match event {
-            MdnsEvent::Discovered(discovered_list) => {
-                for (peer, _addr) in discovered_list {
-                    self.floodsub.add_node_to_partial_view(peer);
-                }
-            }
-            MdnsEvent::Expired(expired_list) => {
-                for (peer, _addr) in expired_list {
-                    if !self.mdns.has_node(&peer) {
-                        self.floodsub.remove_node_from_partial_view(&peer);
-                    }
-                }
-            }
-        }
+
+impl From<MdnsEvent> for StoryBehaviourEvent {
+    fn from(event: MdnsEvent) -> Self {
+        StoryBehaviourEvent::Mdns(event)
     }
 }
+
+//impl NetworkBehaviourEventProcess<FloodsubEvent> for StoryBehaviour {
+//    fn inject_event(&mut self, event: FloodsubEvent) {
+//        match event {
+//            FloodsubEvent::Message(msg) => {
+//                if let Ok(resp) = serde_json::from_slice::<ListResponse>(&msg.data) {
+//                    if resp.receiver == PEER_ID.to_string() {
+//                        info!("Response from {}:", msg.source);
+//                        resp.data.iter().for_each(|r| info!("{:?}", r));
+//                    }
+//                } else if let Ok(req) = serde_json::from_slice::<ListRequest>(&msg.data) {
+//                    match req.mode {
+//                        ListMode::ALL => {
+//                            info!("Received ALL req: {:?} from {:?}", req, msg.source);
+//                            respond_with_public_stories(
+//                                self.response_sender.clone(),
+//                                msg.source.to_string(),
+//                            );
+//                        }
+//                        ListMode::One(ref peer_id) => {
+//                            if peer_id == &PEER_ID.to_string() {
+//                                info!("Received req: {:?} from {:?}", req, msg.source);
+//                                respond_with_public_stories(
+//                                    self.response_sender.clone(),
+//                                    msg.source.to_string(),
+//                                );
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//            _ => (),
+//        }
+//    }
+//}
+//impl NetworkBehaviourEventProcess<MdnsEvent> for StoryBehaviour {
+//    fn inject_event(&mut self, event: MdnsEvent) {
+//    }
+//}
 
 
 fn respond_with_public_stories(sender: mpsc::UnboundedSender<ListResponse>, receiver: String) {
@@ -232,9 +238,16 @@ async fn main() {
             tokio::select! {
                 line = stdin.next_line() => Some(EventType::Input(line.expect("can get line").expect("can read line from stdin"))),
                 response = response_rcv.recv() => Some(EventType::Response(response.expect("response exists"))),
+                //event = SwarmEvent::Behaviour(StoryBehaviourEvent::Floodsub(evt)) => Some(EventType::FloodsubEvent(evt)),
                 event = swarm.select_next_some() => {
-                    info!("Unhandled Swarm Event: {:?}", event);
-                    None
+                    match event {
+                        SwarmEvent::Behaviour(StoryBehaviourEvent::Floodsub(event)) => Some(EventType::FloodsubEvent(event)),
+                        SwarmEvent::Behaviour(StoryBehaviourEvent::Mdns(event)) => Some(EventType::MdnsEvent(event)),
+                        _ => {
+                            info!("Unhandled Swarm Event: {:?}", event);
+                            None
+                        }
+                    }
                 },
             }
         };
@@ -255,6 +268,25 @@ async fn main() {
                     cmd if cmd.starts_with("publish s") => handle_publish_story(cmd).await,
                     _ => error!("unknown command"),
                 },
+                EventType::MdnsEvent(MdnsEvent) => {
+                    match event {
+                        MdnsEvent::Discovered(discovered_list) => {
+                            for (peer, _addr) in discovered_list {
+                                behaviour.floodsub.add_node_to_partial_view(peer);
+                            }
+                        }
+                        MdnsEvent::Expired(expired_list) => {
+                            for (peer, _addr) in expired_list {
+                                if !behaviour.mdns.has_node(&peer) {
+                                    behaviour.floodsub.remove_node_from_partial_view(&peer);
+                                }
+                            }
+                        }
+                    }
+                },
+                EventType::FloodsubEvent(FloodsubEvent) => {
+
+                }
             }
         }
     }
